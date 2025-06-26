d3952ac8cd399114ae63a310cbf41b8dab12d293
semizemes <soatmurodovabbos@gmail.com>
2025-06-27 01:08:47 +0500

    Prepare for deploy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import db from './db.js';
 
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -77,6 +77,6 @@ app.delete('/todos/:id', async (req, res) => {
     }
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
