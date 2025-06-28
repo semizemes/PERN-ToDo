d44b3e09bed1671cf84ac8092e3bc41132983bc4
semizemes <soatmurodovabbos@gmail.com>
2025-06-28 15:52:03 +0500

    Fix API urls

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import db from './db.js';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
